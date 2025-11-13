5e93b9d7d765f9b018e2dc7b173cb47a19a92d76
Ananda S (RPi) <4833102+asarangaram@users.noreply.github.com>
2025-11-13 12:11:33 +0530

    update personal info

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="cl_ml_tools",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A generic framework for building visual search engines with pluggable backends.",
+    author="Ananda Sarangaram",
+    author_email="asarangaram@gmail.com",
+    description="A machine learning toolset for embedded / edge devices.",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
-    url="http://your-repository-url.com",
+    url="http://anandas.in",
     packages=['cl_ml_tools'],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -18,6 +18,5 @@ setup(
     python_requires='>=3.6',
     install_requires=[
         "numpy",
-        "pillow",
     ],
 )
